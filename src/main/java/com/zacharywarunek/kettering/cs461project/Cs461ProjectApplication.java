0c8f2e79860a8170a10d609d413dfe141cc7c076
zwarunek <zacharywarunek8@gmail.com>
2021-03-01 17:12:03 -0500

    Initial commit

---
+++ src/main/java/com/zacharywarunek/kettering/cs461project/Cs461ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zacharywarunek.kettering.cs461project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs461ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs461ProjectApplication.class, args);
+    }
+
+}
