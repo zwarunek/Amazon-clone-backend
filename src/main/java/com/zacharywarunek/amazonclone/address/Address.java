8ac57b292b6093dd4f7b81cc8988437b45ec2b2e
zwarunek <zacharywarunek8@gmail.com>
2021-12-18 00:53:04 -0500

    Address Class done

--- src/main/java/com/zacharywarunek/amazonclone/address/Address.java
+++ src/main/java/com/zacharywarunek/amazonclone/address/Address.java
@@ -1,20 +1,47 @@
 package com.zacharywarunek.amazonclone.address;
 
+import com.zacharywarunek.amazonclone.account.Account;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class Address {
+public class Address implements Serializable {
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+    private String city;
+    private String state;
+    private int zipcode;
+    private boolean favorite;
+    private String first_name;
+    private String last_name;
+
+    public Address(Account account, String city, String state, int zipcode, boolean favorite, String first_name,
+                   String last_name) {
+
+        this.account = account;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.favorite = favorite;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+    @Override
+    public String toString() {
+        return String.format(
+                "Address [id=%s, city=%s, state=%s, zipcode=%s, favorite=%s, first_name=%s, last_name=%s]",
+                id, city, state, zipcode, favorite, first_name, last_name);
+    }
 
 }
