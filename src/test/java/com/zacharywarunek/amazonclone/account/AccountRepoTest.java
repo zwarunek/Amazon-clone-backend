ca0c34ed60727c878893a5cd4799fe6e76744c2b
zwarunek <zacharywarunek8@gmail.com>
2021-12-21 10:59:46 -0500

    Repository Tests

--- src/test/java/com/zacharywarunek/amazonclone/account/AccountRepoTest.java
+++ src/test/java/com/zacharywarunek/amazonclone/account/AccountRepoTest.java
@@ -1,78 +1,90 @@
 package com.zacharywarunek.amazonclone.account;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class AccountRepoTest {
 
-    @Autowired
-    private AccountRepo accountRepo;
+  @Autowired private AccountRepo accountRepo;
 
-    @AfterEach
-    void tearDown() {
-        accountRepo.deleteAll();
-    }
+  @Test
+  void findAccountByEmailShouldBeTrue() {
+    String email = "Zach@gmail.com";
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            email,
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    Optional<Account> accountOptional = accountRepo.findAccountByUsername(email);
+    assertThat(accountOptional.isPresent()).isTrue();
+    assertThat(accountOptional.get()).isEqualTo(account);
+  }
 
-    @Test
-    void findAccountByEmailShouldBeTrue() {
-        String email = "Zach@gmail.com";
-        Account account =
-                new Account("Zach", "Warunek", email, "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                        AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        Optional<Account> accountOptional = accountRepo.findAccountByUsername(email);
-        assertTrue(accountOptional.isPresent());
-        assertEquals(account, accountOptional.get());
-    }
+  @Test
+  void findAccountByEmailShouldBeFalse() {
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            "Zach@gmail.com",
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    Optional<Account> accountOptional =
+        accountRepo.findAccountByUsername("notInDatabase@gmail.com");
+    assertThat(accountOptional.isPresent()).isFalse();
+  }
 
-    @Test
-    void findAccountByEmailShouldBeFalse() {
-        Account account = new Account("Zach", "Warunek", "Zach@gmail.com",
-                "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW", AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        Optional<Account> accountOptional = accountRepo.findAccountByUsername("notInDatabase@gmail.com");
-        assertFalse(accountOptional.isPresent());
-    }
+  @Test
+  void checkIfEmailExistsShouldBeTrue() {
+    String email = "za@gmail.com";
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            email,
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    assertThat(accountRepo.checkIfUsernameExists(email)).isTrue();
+  }
 
-    @Test
-    void checkIfEmailExistsShouldBeTrue() {
-        String email = "za@gmail.com";
-        Account account =
-                new Account("Zach", "Warunek", email, "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                        AccountRole.ROLE_USER);
-        accountRepo.save(account);
+  @Test
+  void checkIfEmailExistsShouldBeFalse() {
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            "za@gmail.com",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    assertThat(accountRepo.checkIfUsernameExists("notInDatabase@gmail.com")).isFalse();
+  }
 
-        assertTrue(accountRepo.checkIfUsernameExists(email));
-    }
+  @Test
+  void enableAccount() {
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            "za@gmail.com",
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    accountRepo.enableAccount(account.getUsername());
 
-    @Test
-    void checkIfEmailExistsShouldBeFalse() {
-        Account account = new Account("Zach", "Warunek", "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                "za@gmail.com", AccountRole.ROLE_USER);
-        accountRepo.save(account);
-
-        assertFalse(accountRepo.checkIfUsernameExists("notInDatabase@gmail.com"));
-    }
-
-    @Test
-    void enableAccount() {
-        Account account = new Account("Zach", "Warunek", "za@gmail.com",
-                "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW", AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        accountRepo.enableAccount(account.getUsername());
-
-        Optional<Account> accountOptional = accountRepo.findById(account.getId());
-
-        assertTrue(accountOptional.isPresent());
-        assertTrue(accountOptional.get().isEnabled());
-    }
+    Optional<Account> accountOptional = accountRepo.findById(account.getId());
+    assertThat(accountOptional.isPresent()).isTrue();
+    assertThat(accountOptional.get().isEnabled()).isTrue();
+  }
 }
--- src/test/java/com/zacharywarunek/amazonclone/address/AddressRepoTest.java
+++ src/test/java/com/zacharywarunek/amazonclone/address/AddressRepoTest.java
@@ -3,7 +3,6 @@ package com.zacharywarunek.amazonclone.address;
 import com.zacharywarunek.amazonclone.account.Account;
 import com.zacharywarunek.amazonclone.account.AccountRepo;
 import com.zacharywarunek.amazonclone.account.AccountRole;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,94 +16,103 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class AddressRepoTest {
 
-    @Autowired
-    AddressRepo addressRepo;
-    @Autowired
-    AccountRepo accountRepo;
+  @Autowired AddressRepo addressRepo;
+  @Autowired AccountRepo accountRepo;
 
-    @AfterEach
-    void tearDown() {
-        addressRepo.deleteAll();
-        accountRepo.deleteAll();
-    }
+  @Test
+  void findAddressByAccount() {
+    String email = "Zach@gmail.com";
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            email,
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    Address address1 =
+        new Address(account, "testAddress1", "testCity1", "MI", 12345, false, "Zach", "Warunek");
+    Address address2 =
+        new Address(account, "testAddress2", "testCity2", "MI", 12345, false, "Zach", "Warunek");
 
-    @Test
-    void findAddressByAccount() {
-        String email = "Zach@gmail.com";
-        Account account = new Account("Zach",
-                                      "Warunek",
-                                      email,
-                                      "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                                      AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        Address address1 = new Address(account, "testAddress1", "testCity1", "MI", 12345, false, "Zach", "Warunek");
-        Address address2 = new Address(account, "testAddress2", "testCity2", "MI", 12345, false, "Zach", "Warunek");
+    addressRepo.save(address1);
+    addressRepo.save(address2);
+    List<Address> addressList = addressRepo.findAddressByAccount(account);
+    assertThat(addressList).containsAll(Arrays.asList(address1, address2));
+  }
 
-        addressRepo.save(address1);
-        addressRepo.save(address2);
-        List<Address> addressList = addressRepo.findAddressByAccount(account);
-        assertThat(addressList).containsAll(Arrays.asList(address1, address2));
-    }
+  @Test
+  void findAddressByAccountEmpty() {
+    String email = "Zach@gmail.com";
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            email,
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    List<Address> addressList = addressRepo.findAddressByAccount(account);
+    assertThat(addressList).isEmpty();
+  }
 
-    @Test
-    void findAddressByAccountEmpty() {
-        String email = "Zach@gmail.com";
-        Account account = new Account("Zach",
-                                      "Warunek",
-                                      email,
-                                      "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                                      AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        List<Address> addressList = addressRepo.findAddressByAccount(account);
-        assertThat(addressList).isEmpty();
-    }
+  @Test
+  void deleteAllByAccount() {
+    String email = "Zach@gmail.com";
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            email,
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    Address address1 =
+        new Address(account, "testAddress1", "testCity1", "MI", 12345, false, "Zach", "Warunek");
+    Address address2 =
+        new Address(account, "testAddress2", "testCity2", "MI", 12345, false, "Zach", "Warunek");
+    Address address3 =
+        new Address(account, "testAddress3", "testCity2", "MI", 12345, false, "Zach", "Warunek");
 
-    @Test
-    void deleteAllByAccount() {
-        String email = "Zach@gmail.com";
-        Account account = new Account("Zach",
-                                      "Warunek",
-                                      email,
-                                      "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                                      AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        Address address1 = new Address(account, "testAddress1", "testCity1", "MI", 12345, false, "Zach", "Warunek");
-        Address address2 = new Address(account, "testAddress2", "testCity2", "MI", 12345, false, "Zach", "Warunek");
-        Address address3 = new Address(account, "testAddress3", "testCity2", "MI", 12345, false, "Zach", "Warunek");
+    addressRepo.save(address1);
+    addressRepo.save(address2);
+    addressRepo.save(address3);
+    addressRepo.deleteAllAtAccount(account);
+    assertThat(addressRepo.findAddressByAccount(account)).isEmpty();
+  }
 
-        addressRepo.save(address1);
-        addressRepo.save(address2);
-        addressRepo.save(address3);
-        addressRepo.deleteAllAtAccount(account);
-        assertThat(addressRepo.findAddressByAccount(account)).isEmpty();
-    }
+  @Test
+  void setAsFavorite() {
+    String email = "Zach@gmail.com";
+    Account account =
+        new Account(
+            "Zach",
+            "Warunek",
+            email,
+            "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
+            AccountRole.ROLE_USER);
+    accountRepo.save(account);
+    Address address1 =
+        new Address(account, "testAddress1", "testCity1", "MI", 12345, true, "Zach", "Warunek");
+    Address address2 =
+        new Address(account, "testAddress2", "testCity2", "MI", 12345, false, "Zach", "Warunek");
+    Address address3 =
+        new Address(account, "testAddress3", "testCity2", "MI", 12345, false, "Zach", "Warunek");
 
-    @Test
-    void setAsFavorite() {
-        String email = "Zach@gmail.com";
-        Account account = new Account("Zach",
-                                      "Warunek",
-                                      email,
-                                      "$2a$15$2oqrWMbqoddS.uypTtSXu.xOUlqypXwuocXM4Jb3t1NE4vH.CkuxW",
-                                      AccountRole.ROLE_USER);
-        accountRepo.save(account);
-        Address address1 = new Address(account, "testAddress1", "testCity1", "MI", 12345, true, "Zach", "Warunek");
-        Address address2 = new Address(account, "testAddress2", "testCity2", "MI", 12345, false, "Zach", "Warunek");
-        Address address3 = new Address(account, "testAddress3", "testCity2", "MI", 12345, false, "Zach", "Warunek");
-
-        addressRepo.save(address1);
-        addressRepo.save(address2);
-        addressRepo.save(address3);
-        addressRepo.resetFavorite(account);
-        addressRepo.setFavorite(address2.getId());
-        Optional<Address> addressOptional1 = addressRepo.findById(address1.getId());
-        Optional<Address> addressOptional2 = addressRepo.findFavoriteAddressByAccount(account);
-        assertThat(addressOptional1.isPresent()).isTrue();
-        assertThat(addressOptional1.get().getFavorite()).isFalse();
-        assertThat(addressOptional2.isPresent()).isTrue();
-        assertThat(addressOptional2.get())
-                .usingRecursiveComparison().ignoringFields("favorite").isEqualTo(address2);
-        assertThat(addressOptional2.get().getFavorite()).isTrue();
-    }
-
+    addressRepo.save(address1);
+    addressRepo.save(address2);
+    addressRepo.save(address3);
+    addressRepo.resetFavorite(account);
+    addressRepo.setFavorite(address2.getId());
+    Optional<Address> addressOptional1 = addressRepo.findById(address1.getId());
+    Optional<Address> addressOptional2 = addressRepo.findFavoriteAddressByAccount(account);
+    assertThat(addressOptional1.isPresent()).isTrue();
+    assertThat(addressOptional1.get().getFavorite()).isFalse();
+    assertThat(addressOptional2.isPresent()).isTrue();
+    assertThat(addressOptional2.get())
+        .usingRecursiveComparison()
+        .ignoringFields("favorite")
+        .isEqualTo(address2);
+    assertThat(addressOptional2.get().getFavorite()).isTrue();
+  }
 }
